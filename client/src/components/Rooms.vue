96acb0fe1f745d307ade206f6b32d6cf4d6d473d
burn <burn@gamersclub.com.br>
2019-03-01 17:07:41 -0300

    organize project

---
+++ client/src/components/Rooms.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Rooms
+        <Room />
+    </div>
+</template>
+
+<script>
+import Room from './Room.vue'
+export default {
+    components: {
+        Room 
+    }
+}
+</script>
